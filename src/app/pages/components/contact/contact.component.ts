7fbbc5c3d6979f13b48c3d2e506f0a0e48de9026
Matheus Lima <matheusmarinho.lima@hotmail.com>
2023-05-12 21:23:06 -0300

    Reactive Forms Tipados

--- src/app/pages/components/contact/contact.component.ts
+++ src/app/pages/components/contact/contact.component.ts
@@ -1,18 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  public form = this.formBuilder.group({
+    email: ['teste@email.com'],
+    password: [''],
+  });
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    console.log(this.form.value.email);
+
   }
 
 }
